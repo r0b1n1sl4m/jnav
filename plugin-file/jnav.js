87921320ad1d667a6a352e2df7ab24a9774bc22f
Robin Islam <r0b1n1sl4m@gmail.com>
2015-08-11 04:39:30 +0600

    Spelling problem

--- plugin-file/jnav.js
+++ plugin-file/jnav.js
@@ -2,7 +2,7 @@
 Plugin Name: jNav
 Version: 1.0
 Plugin URI: https://github.com/r0b1n1sl4m
-Description: jNav is a simply very good solution for your responsive site navigation. It's will help you to add a slick mobile nab on your site.
+Description: jNav is a simply very good solution for your responsive site navigation. It's will help you to add a slick mobile nav on your site.
 Author Name: Robin Islam
 Author URI: http://codefairbd.com
 */
