6a7eca40f8aa7ac2c264f5ef94d3fd4ec41a5a13
Robin Islam <r0b1n1sl4m@gmail.com>
2015-08-11 03:39:01 +0600

    Added jnav.js file in plugin folder. all plugin functions will be within this file and also linked with demo.html

---
+++ plugin-file/jnav.js
@@ -0,0 +1,17 @@
+/*
+Plugin Name: jNav
+Version: 1.0
+Plugin URI: https://github.com/r0b1n1sl4m
+Description: jNav is a simply very good solution for your responsive site navigation. It's will help you to add a slick mobile nab on your site.
+Author Name: Robin Islam
+Author URI: http://codefairbd.com
+*/
+
+(function ($) {
+
+    // start plugin functions
+    $.fn.jNav = function (options) {
+		alert("HELLO");
+    }
+
+})(jQuery);
