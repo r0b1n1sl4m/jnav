4c948a853c9e40e9eef629e8bb9a66a03a452f88
Robin Islam <r0b1n1sl4m@gmail.com>
2015-08-11 04:02:49 +0600

    Active another menu for multiple element support testing

--- js/main.js
+++ js/main.js
@@ -2,4 +2,7 @@ $( document ).ready(function() {
     $("#mobile_menu_ul").jNav({
 		clickbar : "#toggle_click"
 	});
+    $("#another_mobile_menu_ul").jNav({
+		clickbar : "#another_toggle_click"
+	});
 });
