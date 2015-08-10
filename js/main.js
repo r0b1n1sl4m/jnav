697126e8c68a02bd543f645dc35bb60a72840015
Robin Islam <r0b1n1sl4m@gmail.com>
2015-08-11 04:05:38 +0600

    Test all other options

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ $( document ).ready(function() {
 		clickbar : "#toggle_click"
 	});
     $("#another_mobile_menu_ul").jNav({
-		clickbar : "#another_toggle_click"
+		clickbar : "#another_toggle_click",
+		closedSymbol : "-",
+		openedSymbol : "+",
+		speed : "slow",
 	});
 });
